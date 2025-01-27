990985663e7eb0b1034858ddd8e69f2955730058
Richard Watts <richard@zilliqa.com>
2025-01-27 10:12:56 +0000

    (fix) Fix error message names

--- packages/plugin-zilliqa/src/index.ts
+++ packages/plugin-zilliqa/src/index.ts
@@ -7,7 +7,7 @@ export async function zilliqaPlugin(
 ): Promise<Plugin> {
     const zilliqaWalletClient = await getZilliqaWalletClient(getSetting);
     if (!zilliqaWalletClient) {
-      throw new Error("Zilliqa wallet client initialization failed. Ensure that EVM_PRIVATE_KEY and EVM_PROVIDER_URL are configured.");
+      throw new Error("Zilliqa wallet client initialization failed. Ensure that ZILLIQA_PRIVATE_KEY and ZILLIQA_PROVIDER_URL are configured.");
     }
     const walletClient = zilliqaWalletClient!.getEVM();
     const actions = await getOnChainActions(walletClient, zilliqaWalletClient!);
